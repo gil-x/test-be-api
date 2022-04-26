3b7a364291dedce93235f9cd52c31cfe4e41a73a
gil-x <gino.ladowitch@gmail.com>
2022-04-26 10:12:45 +0200

    Lot of things...

--- index.php
+++ index.php
@@ -17,14 +17,14 @@
 
     <div class="page">
         <header class="header">
-            <a href="/">
-                <?php
-            include 'svg/logo_txt.svg';?>
-                <img class="logo" src="media/logo.png" alt="Logo Lambda" />
-                <span class="logo_text">Productly</span>
+            <a class="logo" href="/">
+                <img class="logo_icon" src="media/logo.png" alt="" />
+                <!-- <span class="logo_text">Productly</span> -->
+                <img class="logo_text-svg" src="svg/logo_text.svg" alt="">
+                <?php // include 'svg/logo_text.svg'; ?>
             </a>
             <nav class="main-menu">
-                <ul>
+                <ul class="main-menu_list">
                     <li class="menu-item">
                         <a class="link" href=#>Product</a>
                     </li>
@@ -37,10 +37,10 @@
                     <li class="menu-item">
                         <a class="link" href=#>Resources</a>
                     </li>
-                    <li class="menu-item special">
+                    <li class="menu-item sign-in">
                         <a class="link" href=#>Sign In</a>
                     </li>
-                    <li class="menu-item special">
+                    <li class="menu-item sign-up">
                         <a class="link" href=#>Sign Up</a>
                     </li>
                 </ul>
@@ -48,11 +48,16 @@
         </header>
 
         <section class="section introduction">
-            <h1 class="h1">The Design Thinking superpowers</h1>
-            <p class="baseline">Tools, tutorials, design and innovation experts, all in one place! The most intuitive way to imagine your next user experience.</p>
-            <div class="call-to-action">
-                <button class="button get-started">Get started</button>
-                <button class="button watch-video">Watch the Video</button>
+            <div class="text">
+                <h1 class="h1">
+                    <span class="h1-part">The Design Thinking </span>
+                    <span class="h1-part">superpowers</span>
+                </h1>
+                <p class="introduction_baseline">Tools, tutorials, design and innovation experts, all in one place! The most intuitive way to imagine your next user experience.</p>
+                <div class="call-to-action">
+                    <button class="button get-started">Get started</button>
+                    <button class="button watch-video">Watch the Video</button>
+                </div>
             </div>
             <div class="illustration">
                 <img class="svg-picture" src="svg/picture0.svg" alt="">
@@ -60,7 +65,7 @@
             <?php //include 'svg/picture0.svg'; ?>
         </section>
 
-        <section class="features">
+        <section class="section features">
             <h2 class="h2">We design tools to unveil your superpowers</h2>
             <ul class="features_list">
                 <li class="feature_item">
@@ -87,7 +92,7 @@
 
         <main class="main">
 
-            <section class="section design-professionals">
+            <section class="section illustrated">
                 <h2 class="title">
                     <span class="small">Effortless Validation for</span>
                     Design Professionals
@@ -102,33 +107,149 @@
                     <p>If you are looking for a new way to promote your business that won’t cost you more money,</p>
                 </div>
                 <div class="illustration">
-                    <?php // include 'svg/picture1.svg'; ?>
                     <img class="svg-picture" src="svg/picture1.svg" alt="">
                 </div>
             </section>
 
-            <section class="super-heroes">
+            <section class="section illustrated">
+                <h2 class="title">
+                    <span class="small">Easier decision making for</span>
+                    Product Managers
+                </h2>
+                <div class="text">
+                    <p>The Myspace page defines the individual, his or her characteristics, traits, personal choices and the overall personality of the person.</p>
+                    <ul>
+                        <li>Never worry about overpaying for your energy again.</li>
+                        <li>We will only switch you to energy companies that we trust and will treat you right</li>
+                        <li>We track the markets daily and know where the savings are.</li>
+                    </ul>
+                </div>
+                <div class="illustration">
+                    <img class="svg-picture" src="svg/picture2.svg" alt="">
+                </div>
+            </section>
+
+            <section class="section illustrated">
+                <h2 class="title">
+                    <span class="small">Optimisation for</span>
+                    Marketers
+                </h2>
+                <div class="text">
+                    <p>Few would argue that, despite the advancements of feminism over the past three decades, women still face a double standard when it comes to their behavior.</p>
+                    <h3>Accessory makers</h3>
+                    <p>While most people enjoy casino gambling, sports betting, lottery and bingo playing for the fun</p>
+                    <h3>Alterationists</h3>
+                    <p>If you are looking for a new way to promote your business that won’t cost you more money</p>
+                    <h3>Custom Design designers</h3>
+                    <p>If you are looking for a new way to promote your business that won’t cost you more money</p>
+                </div>
+                <div class="illustration">
+                    <img class="svg-picture" src="svg/picture1.svg" alt="">
+                </div>
+            </section>
+
+            <section class="section super-heroes">
                 <h2>Need a super hero?</h2>
                 <p>Do you require some help for your project: Conception workshop,<br>
                 prototyping, marketing strategy, landing page, Ux/UI?</p>
                 <div class="call-to-action">
-                    <button class="button get-started">Get started</button>
-                    <button class="button watch-video">Watch the Video</button>
+                    <button class="button contact-expert">Contact our expert</button>
                 </div>
+                <ul>
+                    <li class="heroe">
+                        <span class="bubble">I can take care of your pitch</span>
+                        <img src="media/person1.png" alt="" class="avatar">
+                    </li>
+                    <li class="heroe">
+                        <span class="bubble">I can design you website</span>
+                        <img src="media/person2.png" alt="" class="avatar">
+                    </li>
+                    <li class="heroe">
+                        <span class="bubble">I will define the profile of your users</span>
+                        <img src="media/person3.png" alt="" class="avatar">
+                    </li>
+                    <l class="heroe"i>
+                        <span class="bubble">I can prototype your app</span>
+                        <img src="media/person4.png" alt="" class="avatar">
+                    </li>
+                    <li class="heroe">
+                        <span class="bubble">I can help marketing strategy</span>
+                        <img src="media/person5.png" alt="" class="avatar">
+                    </li>
+                </ul>
             </section>
 
-            <section class="slideshow"></section>
+            <section class="section slideshow">
+                <div class="slideshow_container">
+                    <div class="slide-item">
+                        <img class="slide_picture" src="media/slide1.png" alt="">
+                        <span class="info">By Abdullah |  03 March 2019</span>
+                        <h3 class="h3">Increasing Prosperity With Positive Thinking</h3>
+                    </div>
+                    <div class="slide-item">
+                        <img class="slide_picture" src="media/slide2.png" alt="">
+                        <span class="info">By Abdullah |  03 March 2019</span>
+                        <h3 class="h3">Increasing Prosperity With Positive Thinking</h3>
+                    </div>
+                    <div class="slide-item">
+                        <img class="slide_picture" src="media/slide3.png" alt="">
+                        <span class="info">By Abdullah |  03 March 2019</span>
+                        <h3 class="h3">Increasing Prosperity With Positive Thinking</h3>
+                    </div>
+                    <div class="slide-item">
+                        <img class="slide_picture" src="media/slide4.png" alt="">
+                        <span class="info">By Abdullah |  03 March 2019</span>
+                        <h3 class="h3">Increasing Prosperity With Positive Thinking</h3>
+                    </div>
+                </div>
+                <div class="bullets">
+                    <nav>
+                        <span class="bullet active"></span>
+                        <span class="bullet"></span>
+                        <span class="bullet"></span>
+                    </nav>
+                </div>
+
+            </section>
 
         </main>
 
         <footer class="footer">
-            <div class="footer_logo"></div>
-            <div class="quick-links"></div>
-            <div class="legal-stuff"></div>
-            <div class="call-to-action">
-                <p>knowing you're always on the best energy deal.</p>
-                <input type="text" placeholder="Enter your phone Number">
-                <button class="button sign-up-now">Sign up Now</button>
+            <div class="column footer_logo">
+                <a class="logo" href="/">
+                    <img class="logo_icon" src="media/logo.png" alt="Logo Lambda" />
+                    <img class="logo_text-svg" src="svg/logo_text.svg" alt="">
+                </a>
+            </div>
+            <div class="column footer-menu quick-links">
+                <span class="footer-menu_title" >Quick Links</span>
+                <nav>
+                    <ul class="footer-menu_list">
+                        <li class="menu-item">About Us</li>
+                        <li class="menu-item">Blog</li>
+                        <li class="menu-item">Contact</li>
+                        <li class="menu-item">FAQ</li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="column footer-menu legal-stuff">
+                <span class="footer-menu_title" >Quick Links</span>
+                <nav>
+                    <ul class="footer-menu_list">
+                        <li class="menu-item">Disclaimer</li>
+                        <li class="menu-item">Financing</li>
+                        <li class="menu-item">Privacy Policy</li>
+                        <li class="menu-item">Terms of Service</li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="column footer-menucall-to-action">
+                <p class="footer-menu_title">knowing you're always on the best energy deal.</p>
+                <form class="sign-up-form" action="">
+                    <input class="phone-field" type="text" placeholder="Enter your phone Number">
+                    <button class="button sign-up-now" type="submit">Sign up Now</button>
+                </form>
+                
             </div>
         </footer>
     </div>
